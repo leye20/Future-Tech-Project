cc47115ce5e73bc7112422a788e72438b8e878f3
Taiwo Adeniji <taiwoadeniji19@gmail.com>
2020-04-25 04:04:23 -0600

    signup button submits inputs

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {RootState} from '../store';
-import {addNewSignupToUsersList, checkUsersListForExistingUsername} from '../store/signup/action';
+import {addNewSignupToUsersList} from '../store/signup/action';
 import {UsersList} from '../store/signup/types';
-import { Button, Checkbox, Form, Grid } from 'semantic-ui-react';
+import { Button, Form, Grid } from 'semantic-ui-react';
 import {connect} from 'react-redux';
 
+// declaring ISignupPageProps as my props
 export interface ISignupPageProps {
     addNewSignupToUsersList: typeof addNewSignupToUsersList,
-    checkUsersListForExistingUsername: typeof checkUsersListForExistingUsername,
-    userslist: UsersList []
+       userslist: UsersList []
 }
 
 export class SignupPage extends React.Component<ISignupPageProps>
@@ -19,46 +19,54 @@ export class SignupPage extends React.Component<ISignupPageProps>
         return randomNumber;
     }
  
-
+// declaring the eventhandler and setting it to default
 SignedUp = ( event: any ) => {
     event.preventDefault();
-    // Handle retrieval of form field value.
+
+    // Handle retrieval of username form field value.
     const userField: HTMLInputElement | null = document.querySelector( '[name="username"]' );
     let userFieldValue: string = '';
     if ( userField !== null ) userFieldValue = userField.value;
-    // Add new item to inventory.
+    
+    // Add new username to userslist.
     this.props.addNewSignupToUsersList( {
         id: this.generateID(),
         name: userFieldValue
     });
+    // Handle retrieval of password form field value.
     const passField: HTMLInputElement | null = document.querySelector( '[name="password"]' );
     let passFieldValue: string = '';
     if ( passField !== null ) passFieldValue = passField.value;
-    // Add new item to inventory.
+   
+    // Add new password to userslist..
     this.props.addNewSignupToUsersList( {
         id: this.generateID(),
         name: passFieldValue
     });
+
+    // Handle retrieval of interests form field value.
     const intField: HTMLInputElement | null = document.querySelector( '[name="interests"]' );
     let intFieldValue: string = '';
     if ( intField !== null ) intFieldValue = intField.value;
-    // Add new item to inventory.
+    
+    // Add new intersets to userslist.
     this.props.addNewSignupToUsersList( {
         id: this.generateID(),
         name: intFieldValue
     });
+
+    // show alert on successfull signup
+    alert("Welcome!!! You have successfully signed up."); 
+
+    // Clear input fields and reset them to blank
+    event.target.reset();
+  }
    
- console.log(userField,  passField, intField );
-
-    if ( userField !== null ) userField.value = '';
-    if ( passField !== null ) passField.value = '';
-    if ( intField !== null ) intField.value = '';
-
-}
 
 render() {
     let {userslist}=this.props;
-    console.log (userslist);
+    console.log (userslist); // checking the console to ensure that new imputs are added 
+
     return (
 <Grid centered>
   <Form onSubmit= {this.SignedUp}>
@@ -77,9 +85,6 @@ render() {
       <label>Interests</label>
       <input name= 'interests' placeholder='Interests' />
     </Form.Field>
-    <Form.Field>
-      <Checkbox label='I agree to the Terms and Conditions' />
-    </Form.Field>
     <Button type='submit'>Signup</Button>
   </Form>
   </Grid>
@@ -87,14 +92,15 @@ render() {
 }
 }
 
-  
+// Retrieve "userslist" from our "global" redux state.
 const mapStateToProps = (state: RootState) => {
     return {
         userslist: state.signup.userslist
     }
 }
 
+// Connecting Redux and React
 export default connect(
     mapStateToProps,
-    { addNewSignupToUsersList, checkUsersListForExistingUsername}
+    { addNewSignupToUsersList}
 )(SignupPage);
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import {combineReducers, createStore} from 'redux';
 import {signupReducer} from './signup/reducer';
 
+
  const rootReducer = combineReducers({
   signup: signupReducer
+  
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
