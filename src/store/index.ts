2793a143d2129fc0943452faf7cbf0f0826120b0
ouatarak <58311093+ouatarak@users.noreply.github.com>
2020-04-27 14:45:24 -0600

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,11 @@
 import { combineReducers, createStore } from 'redux';
 import { newsfeedReducer } from './newsfeed/newsfeed_reducer';
+import { profileReducer } from './profile/kounandi_reducer';
 
 const rootReducer = combineReducers( {
-    newsfeed: newsfeedReducer
+    newsfeed: newsfeedReducer,
+    profile: profileReducer,
+    
 } );
 
 export type RootState = ReturnType<typeof rootReducer>;
