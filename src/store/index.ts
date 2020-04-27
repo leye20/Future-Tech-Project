3aff162e42d90efc60d800c0787190dcbf039ae9
taiwoadeniji <61204662+taiwoadeniji@users.noreply.github.com>
2020-04-27 14:58:46 -0600

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers, createStore } from 'redux';
 import { newsfeedReducer } from './newsfeed/newsfeed_reducer';
 import { profileReducer } from './profile/kounandi_reducer';
+import {signupReducer} from './signup/Taiwo_reducer';
 
 const rootReducer = combineReducers( {
     newsfeed: newsfeedReducer,
     profile: profileReducer,
+    signup: signupReducer,
     
 } );
 
