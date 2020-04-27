6c95981ccee9cba7c1cd377aed3e4e88ccba54b8
sijosiji <60945270+sijosiji@users.noreply.github.com>
2020-04-28 02:40:24 +0530

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,15 @@ import { combineReducers, createStore } from 'redux';
 import { newsfeedReducer } from './newsfeed/newsfeed_reducer';
 import { profileReducer } from './profile/kounandi_reducer';
 import {signupReducer} from './signup/Taiwo_reducer';
+import { sessionReducer } from "./session/sijo_reducers";
+import { userReducer } from "./Users/sijo_reducers";
 
 const rootReducer = combineReducers( {
     newsfeed: newsfeedReducer,
     profile: profileReducer,
     signup: signupReducer,
-    
+    session: sessionReducer,
+    userlist: userReducer
 } );
 
 export type RootState = ReturnType<typeof rootReducer>;
